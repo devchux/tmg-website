58ed826547d321c6d1adf37d40ca57e92b1f1a05
devchux <chukwudieze97@gmail.com>
2022-09-25 13:44:40 +0100

    add partners

--- components/home/partners.tsx
+++ components/home/partners.tsx
@@ -1,4 +1,5 @@
 import Heading from "components/typography/Heading";
+import Image from "next/image";
 import React from "react";
 import { ReactSVG } from "react-svg";
 import styles from "styles/home/partners.module.scss";
@@ -8,11 +9,27 @@ const Partners = () => {
     <div className={styles.partners}>
       <Heading>Our Partners</Heading>
       <div className={styles.wrapper}>
-        <ReactSVG src="/assets/svg/company-text.svg" />
-        <ReactSVG src="/assets/svg/company-tagline.svg" />
-        <ReactSVG src="/assets/svg/a-company.svg" />
-        <ReactSVG src="/assets/svg/text-logo.svg" />
-        <ReactSVG src="/assets/svg/company-tagline.svg" />
+        <Image
+          src="/assets/images/partners/1.png"
+          alt=""
+          height="63"
+          width="158"
+          style={{ background: "white" }}
+        />
+        <Image
+          src="/assets/images/partners/3.png"
+          alt=""
+          height="195"
+          width="256"
+        />
+        <Image
+          src="/assets/images/partners/2.jpg"
+          alt=""
+          height="156"
+          width="256"
+        />
+        {/* <ReactSVG src="/assets/svg/text-logo.svg" />
+        <ReactSVG src="/assets/svg/company-tagline.svg" /> */}
       </div>
     </div>
   );
