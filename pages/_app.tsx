8b73c82ec058c1ffb076ee0647d2c3c327e30cf6
Chukwudi Eze <chukwudieze97@gmail.com>
2023-10-19 11:24:36 +0100

    add metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import Footer from "components/footer";
 import Navbar from "components/nav/navbar";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Fragment } from "react";
 import { NotificationContainer } from "react-notifications";
 import "react-notifications/lib/notifications.css";
@@ -9,6 +10,9 @@ import "styles/global.scss";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
+      <Head>
+        <title>Triphecta Management Group</title>
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
